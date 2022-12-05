1ebdd3b4e9b7375d33994b5436f9404d64f165cb
iurii <furmaniurii@gmail.com>
2022-12-05 13:00:41 +0400

    Add basic gamelogic

--- src/main.js
+++ src/main.js
@@ -2,18 +2,93 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   const doodler = document.createElement('div');
-  let doodlerStyleLeft = 50;
-  let doodlerStyleBottom = 150;
+  let doodlerLeftSpace = 50;
+  let doodlerBottomSpace = 201;
+  let isGameOver = false;
+  let platformCount = 5;
+  let platforms = [];
+  let upTimerId;
+  let downTimerId;
 
   function createDoodler() {
     grid.appendChild(doodler);
     doodler.classList.add('doodler');
-    doodler.style.left = doodlerStyleLeft + 'px';
-    doodler.style.bottom = doodlerStyleBottom + 'px';
+    doodler.style.left = doodlerLeftSpace + 'px';
+    doodler.style.bottom = doodlerBottomSpace + 'px';
   };
   createDoodler();
 
-  function start() {
-    createDoodler();
+  class Platform {
+    constructor(newPlatformBottom) {
+        this.bottom = newPlatformBottom;
+        this.left = Math.random() * 315;
+        this.visual = document.createElement('div');
+
+        const visual = this.visual;
+        visual.classList.add('platform');
+        visual.style.left = this.left + 'px';
+        visual.style.bottom = this.bottom + 'px';
+        grid.appendChild(visual);
+    }
   }
+
+  function createPlatforms() {
+    for (let i = 0; i < platformCount; i++) {
+        let platformGap = 600 / 5;
+        let newPlatformBottom = 100 + i * platformGap;
+        let newPlatform = new Platform(newPlatformBottom);
+        platforms.push(newPlatform);
+    }
+  }
+
+  function movePlatforms() {
+    if (doodlerBottomSpace > 200) {
+        platforms.forEach(platform => {
+            platform.bottom -= 4;
+            let visual = platform.visual;
+            visual.style.bottom = platform.bottom + 'px';
+        })
+    }
+  } 
+
+
+  function jump() {
+    clearInterval(downTimerId);
+    upTimerId = setInterval(function() {
+        doodlerBottomSpace += 20;
+        doodler.style.bottom = doodlerBottomSpace + 'px';
+        if (doodlerBottomSpace > 350) {
+            fall();
+        }
+    }, 30);
+  }
+
+  function fall() {
+    clearInterval(upTimerId);
+    downTimerId = setInterval(function() {
+        doodlerBottomSpace -= 5;
+        doodler.style.bottom = doodlerBottomSpace + 'px';
+        if (doodlerBottomSpace <= 0) {
+            gameOver();
+        }
+    }, 30)
+  }
+
+  function gameOver() {
+    console.log('Game Over');
+    isGameOver = true;
+    clearInterval(upTimerId);
+    clearInterval(downTimerId);
+  }
+
+  function start() {
+    if (!isGameOver) {
+        createDoodler();
+        createPlatforms();
+        setInterval(movePlatforms, 30);
+        jump()
+    }
+  }
+  //attach to button
+  start()
 });
